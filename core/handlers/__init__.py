40e5000d17b8eb8c46b0570e14a3e6e9564ba623
hasanbasuwala <hasanbasuwala50@gmail.com>
2026-06-30 14:28:28 +0530

    Update __init__.py

--- core/handlers/__init__.py
+++ core/handlers/__init__.py
@@ -1,5 +1,8 @@
 from .commands import setup_commands
-# We will add media and callbacks in Phase 3.
+from .media import setup_media
+from .callbacks import setup_callbacks
 
 def setup_all_handlers(app):
     setup_commands(app)
+    setup_media(app)
+    setup_callbacks(app)
