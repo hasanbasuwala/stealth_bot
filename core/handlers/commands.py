9ac93fa38d5e40f6c53d4a121f6ade98a089615a
hasanbasuwala <hasanbasuwala50@gmail.com>
2026-06-30 10:01:57 +0530

    Update commands.py

--- core/handlers/commands.py
+++ core/handlers/commands.py
@@ -1,10 +1,17 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from core import state, ui
+from core.loggers.engine_log import get_logger
+
+log = get_logger("handlers")
 
 def setup_commands(app: Client):
-    @app.on_message(filters.command(["start", "dashboard"]) & filters.user(state.OWNER_ID))
+    
+    # Temporarily removed the OWNER_ID filter to test permissions!
+    @app.on_message(filters.command(["start", "dashboard"]))
     async def init_dashboard(_, msg: Message):
+        log.info(f"Dashboard requested by User ID: {msg.from_user.id}")
+        
         state._dashboard_chat_id = msg.chat.id
         m = await msg.reply(
             ui._build_dashboard_text(state._job_list_page),
