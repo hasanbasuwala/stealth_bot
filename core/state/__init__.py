9f0544cf015ff00b2274157e28a8fe46783fa540
hasanbasuwala <hasanbasuwala50@gmail.com>
2026-06-30 14:19:22 +0530

    Update __init__.py

--- core/state/__init__.py
+++ core/state/__init__.py
@@ -1,3 +1,8 @@
 from .constants import *
-from .memory import *
+from .memory import (
+    dl_queue, enc_queue, up_queue,
+    _live_progress, _active_procs, _pending_confirmations,
+    _dashboard_msg_id, _dashboard_chat_id, _job_list_page,
+    JOBS_PER_PAGE, _last_completed
+)
 from .models import Stage, Job, ensure_progress
